2ee88cb1784febfdc2219df03f9a0d4947a515fa
Yixuan Zheng <r4kit1czheng@gmail.com>
2024-06-09 18:13:11 -0400

    upd rabin karp

--- string/RabinCarp_Oracle.cpp
+++ string/RabinCarp_Oracle.cpp
@@ -1,6 +1,7 @@
 class RabinKarp{
     // Need M ~ 2kN^2/4 * log(kN^2/4) for error < 1/k worst case
-    int p;
+    // int p;
+    const int p = 1e9 + 7;
     int n, b;
     vector<int> exp, a, reva;
 
@@ -18,16 +19,16 @@ public:
     }
 
     void init(vector<int>& s) {
-        MillerRabin T;
-        p = T.Generate(1 << 28, 1 << 29);
+        // MillerRabin T;
+        // p = T.Generate(1 << 28, 1 << 29);
         n = s.size();
         a.resize(n + 1);
         exp.resize(n + 1);
         reva.resize(n + 1);
         exp[0] = 1;
-        for (int i = 1; i <= n; i++) exp[i] = ((ll)exp[i - 1] * b) % p;
-        for (int i = 1; i <= n; i++) a[i] = ((ll)a[i - 1] * b + s[i - 1]) % p;
-        for (int i = n - 1; i >= 0; i--) reva[i] = ((ll)reva[i + 1] * b + s[i]) % p;
+        for (int i = 1; i <= n; i++) exp[i] = ((long long)exp[i - 1] * b) % p;
+        for (int i = 1; i <= n; i++) a[i] = ((long long)a[i - 1] * b + s[i - 1]) % p;
+        for (int i = n - 1; i >= 0; i--) reva[i] = ((long long)reva[i + 1] * b + s[i]) % p;
     }
 
     // 0-indexed, exclusive right endpoint
