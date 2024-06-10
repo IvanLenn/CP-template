a355f9cccf584ea56a30a4fdf74bd0f3c0842f57
Bodo <r4kit1czheng@gmail.com>
2024-06-10 17:57:51 -0400

    update segtree

--- ds/segtree_singleupd.cpp
+++ ds/segtree_singleupd.cpp
@@ -1,30 +1,37 @@
-// modify: merge, modify, base case
-const int N = 4e5;  // limit for array size
-int cnt;  // array size
-int t[2 * N];
- 
-inline int merge(int a, int b) {
-    return a + b;
-}
- 
-void build() {  // build the tree
-    for (int i = cnt - 1; i > 0; --i) {
-        t[i] = merge(t[i<<1], t[i<<1|1]);
-    }
-}
- 
-void modify(int p, int value) {  // set value at position p
-    p += cnt;
-    t[p] += value;
- 	for (p; p > 1; p >>= 1) t[p>>1] = merge(t[p], t[p^1]);
-}
- 
-int query(int l, int r) {  // sum on interval [l, r)
-    int resl = 0, resr = 0;
-    for (l += cnt, r += cnt; l < r; l >>= 1, r >>= 1) {
-        if (l&1) resl = merge(resl, t[l++]);
-        if (r&1) resr = merge(t[--r], resr);
-    }
-    return merge(resl, resr);
-}
+template<class F>
+struct segtree{
+    // modify: merge, modify, base case
+    int cnt;  // array size
+    vector<F> t;
 
+    segtree() {}
+    void resize(int n) {
+        cnt = n;
+        t.resize(2 * n);
+    }
+    
+    inline F merge(F a, F b) {
+        return a + b;
+    }
+    
+    void build() {  // build the tree
+        for (int i = cnt - 1; i > 0; --i) {
+            t[i] = merge(t[i<<1], t[i<<1|1]);
+        }
+    }
+    
+    void modify(int p, F value) {  // set value at position p
+        p += cnt;
+        t[p] = value;
+        for (p; p > 1; p >>= 1) t[p>>1] = merge(t[p], t[p^1]);
+    }
+    
+    F query(int l, int r) {  // sum on interval [l, r)
+        F resl = 0, resr = 0;
+        for (l += cnt, r += cnt; l < r; l >>= 1, r >>= 1) {
+            if (l&1) resl = merge(resl, t[l++]);
+            if (r&1) resr = merge(t[--r], resr);
+        }
+        return merge(resl, resr);
+    }
+};
