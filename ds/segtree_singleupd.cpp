50f97d06d8eb643cb76ab582022446ddb57a6ca4
Bodo <r4kit1czheng@gmail.com>
2024-06-25 13:34:39 -0400

    fix segtree associativity bug

--- ds/segtree_singleupd.cpp
+++ ds/segtree_singleupd.cpp
@@ -1,32 +1,29 @@
 template<class F>
 struct segtree{
-    // modify: merge, modify, base case
-    int cnt;  // array size
+    int cnt;
     vector<F> t;
 
-    segtree() {}
-    void resize(int n) {
-        cnt = n;
-        t.resize(2 * n);
+    segtree(int cnt) : cnt(cnt) {
+        t.resize(2 * cnt);
     }
     
     inline F merge(F a, F b) {
         return a + b;
     }
     
-    void build() {  // build the tree
+    void build() {
         for (int i = cnt - 1; i > 0; --i) {
             t[i] = merge(t[i<<1], t[i<<1|1]);
         }
     }
     
-    void modify(int p, F value) {  // set value at position p
+    void modify(int p, F value) {
         p += cnt;
         t[p] = value;
-        for (p; p > 1; p >>= 1) t[p>>1] = merge(t[p], t[p^1]);
+        for (p; p > 1; p >>= 1) t[p>>1] = (p&1?merge(t[p^1], t[p]):merge(t[p], t[p^1]));
     }
     
-    F query(int l, int r) {  // sum on interval [l, r)
+    F query(int l, int r) {
         F resl = 0, resr = 0;
         for (l += cnt, r += cnt; l < r; l >>= 1, r >>= 1) {
             if (l&1) resl = merge(resl, t[l++]);
