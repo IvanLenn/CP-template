8c1ea2e7a985fb9c329cac37bde2da3ad3abba93
Yixuan Zheng <r4kit1czheng@gmail.com>
2024-09-15 14:10:42 -0400

    Update fast range modi segtree for commutative operator

--- ds/segtree_singlequery.cpp
+++ ds/segtree_singlequery.cpp
@@ -2,10 +2,11 @@ template<class F>
 struct segtree{
     // modify: merge, modify, base case
     int cnt;  // array size
+    F id;
     vector<F> t;
 
-    segtree(int cnt) : cnt(cnt) {
-        t.resize(2 * cnt);
+    segtree(int cnt, F& id) : cnt(cnt), id(id) {
+        t.resize(2 * cnt, id);
     }
     
     inline F merge(F a, F b) {
@@ -14,13 +15,19 @@ struct segtree{
     
     void modify(int l, int r, const F& value) {
         for (l += cnt, r += cnt; l < r; l >>= 1, r >>= 1) {
-            if (l&1) t[l++] += value;
-            if (r&1) t[--r] += value;
+            if (l&1) {
+                t[l] = merge(t[l], value);
+                l++;
+            }
+            if (r&1) {
+                r--;
+                t[r] = merge(t[r], value);
+            }
         }
     }
     
     F query(int p) {
-        F res = 0;
+        F res = id;
         for (p += cnt; p > 0; p >>= 1) res = merge(res, t[p]);
         return res;
     }
